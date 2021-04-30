c9ebe2c764ef4d42f81986affb927323ff8f49d4
Joe <jgalante00@gmail.com>
2021-04-30 12:04:20 -0400

    workbear app and home page started

---
+++ workbear/src/components/NavBar.js
@@ -0,0 +1,18 @@
+//=============== SETUP ===============//
+
+// react router
+import { Link } from 'react-router-dom';
+
+
+//=============== FUNCTION ===============//
+
+const NavBar = () =>
+{
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+        </nav>
+    )
+}
+
+export default NavBar;
---
+++ workbear/src/pages/Home.js
@@ -0,0 +1,12 @@
+
+
+const Home = () =>
+{
+    return (
+        <div>
+            <h1 style={{ font: "48px Arial", fontWeight: "bolder" }}>Welcome to WorkBear</h1>
+        </div>
+    )
+}
+
+export default Home;
